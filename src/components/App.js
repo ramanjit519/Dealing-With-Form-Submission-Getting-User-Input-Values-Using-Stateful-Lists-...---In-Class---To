d7d0aa077fea30d065078d1a7fd823b8ddbe1401
ramanjit519 <43263426+ramanjit519@users.noreply.github.com>
2022-06-20 10:22:21 +0530

    send

--- src/components/App.js
+++ src/components/App.js
@@ -1,71 +1,25 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { useState, useEffect } from "react";
+import "../styles/App.css";
+const App = () => {
+  const [dateTime, setdateTime] = useState("");
+  const updateTime = () => {
+    let tempTime = new Date().toLocaleString();
+    setdateTime(tempTime);
+  };
+  useEffect(() => {
+    let tempTime = new Date().toLocaleString();
+    setdateTime(tempTime);
+    let intrval = setInterval(updateTime, 1000);
+    return () => {
+      clearInterval(intrval);
+    };
+  }, []);
 
-class App extends Component {
-	constructor(props)
-	{
-		super(props);
-		this.state = {
-			list : [
-				{
-					name : "naman",
-					percent : 60.0000
-				},
-				{
-					name : "ritesh",
-					percent : 99.9431
-				},
-				{
-					name : "hitesh",
-					percent : 65.3365
-				},
-				{
-					name : "shreya",
-					percent : 80.6400
-				},
-				{
-					name : "ankur",
-					percent : 78.8970
-				},
-				{
-					name : "vanshu",
-					percent : 78.9876
-				},
-				{
-					name : "krupal",
-					percent : 85.3214
-				},
-			],
-		}
-	};
-    render() {
-    	return(
-    		<div id="main">
-    			{
-    				//Correct the percentage conditional for correct output
-    				this.state.list.map(function(item,index){
-				   		if(item.percent > 0)
-				   		{
-				   			return(
-				   				//Conditional statement in the className of below div to give callName="bg-pink"
-				   				//if the percent >= 90 else className = "" 
-				   				<div key={index} className={}>
-					   				<div className="name">
-					   					 //Write code to display name
-					   				</div>
-					   				<div className="percent">
-					   					 //Write code to display percentage upto 2 decimal places
-					   				</div>
-				   				</div>
-				   				);
-				   		}
-			       })
-			    }
-    		</div>
-    		);
-    }
-}
-
+  return (
+    <div id="main">
+      <div className="date-time">{dateTime}</div>
+    </div>
+  );
+};
 
 export default App;
-
